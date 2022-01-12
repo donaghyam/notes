a455293f37d9e494a4112e2e7701b91685fd56e9
Adam Donaghy <donaghyam@gmail.com>
2022-01-12 11:05:01 -0600

    adding initial notes

---
+++ notes.js
@@ -0,0 +1,18 @@
+const notes = [
+    {
+        id: 1,
+        subject: "for...of loops",
+        date: "1/12/22",
+        feeling: "Not very confident yet",
+        timeSpent: 30
+    },
+    {
+        id: 2,
+        subject: "functions",
+        date: "1/12/22",
+        feeling: "Not very confident yet",
+        timeSpent: 60
+    }
+]
+
+console.log(notes)
