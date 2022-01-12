29a2bee4eaee5e49cd731256c92ea8cc1389898b
Adam Donaghy <donaghyam@gmail.com>
2022-01-12 11:25:06 -0600

    added for...of loop

--- notes.js
+++ notes.js
@@ -3,14 +3,14 @@ const notes = [
         id: 1,
         subject: "for...of loops",
         date: "1/12/22",
-        feeling: "Not very confident yet",
+        feeling: "not very confident yet",
         timeSpent: 30
     },
     {
         id: 2,
         subject: "functions",
         date: "1/12/22",
-        feeling: "Not very confident yet",
+        feeling: "not very confident yet",
         timeSpent: 60
     }
 ]
@@ -20,11 +20,21 @@ const noteAboutToday = [
     id: 3,
     subject: "array",
     date: "1/12/22",
-    feeling: "Confident",
+    feeling: "confident",
     timeSpent: 15
     }
 ]
 
 notes.push(noteAboutToday)
 
-console.log(notes)
+for (const note of notes){
+    console.log(`Note ${note.id} 
+    ${note.date}
+    I learned about ${note.subject}
+    I spent ${note.timeSpent} minutes working on it
+    I felt ${note.feeling}.
+    -----------------
+    `)
+}
+
+
